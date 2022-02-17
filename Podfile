5bcb31ca7d3aa6220dee41ffb8f1473c665f53be
ZongAng123 <zongang15@yahoo.com>
2022-02-17 14:33:44 +0800

    复选框

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git’
+platform :ios, '10.0'
+
+target 'SelectXZAndQx' do
+  use_frameworks!
+
+pod 'BEMCheckBox'
+
+end
--- SelectXZAndQx/ViewController.m
+++ SelectXZAndQx/ViewController.m
@@ -6,8 +6,9 @@
 //
 
 #import "ViewController.h"
+#import "BEMCheckBox.h"
 
-@interface ViewController ()
+@interface ViewController ()<BEMCheckBoxDelegate>
 
 @end
 
@@ -15,8 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    self.view.backgroundColor =[UIColor redColor];
     // Do any additional setup after loading the view.
+    //********************** 用户协议 **************
+    BEMCheckBox *userAagreement = [[BEMCheckBox alloc] initWithFrame:CGRectMake(50, 100, 40, 40)];
+    userAagreement.delegate = self;
+    userAagreement.boxType = BEMBoxTypeSquare;
+    [self.view addSubview:userAagreement];
+    
+    
 }
+
+- (void)didTapCheckBox:(BEMCheckBox *)checkBox{
+     NSLog(@"%@", checkBox.on ? @"YES":@"NO");
+}
+
 
 
 @end
